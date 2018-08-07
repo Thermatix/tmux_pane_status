2d8e91631e631af0bf311f8c69eb8deaf7cfc262
thermatix <mbeckerwork@gmail.com>
2018-08-07 23:31:03 +0100

    adds basic build status funcationality

--- lib/functions.zsh
+++ lib/functions.zsh
@@ -1,24 +1,34 @@
+#!/usr/bin/env zsh
+displays_dir=$1
+function set_tmux_opt {
+  tmux set -q "@$1_$TMUX_PANE" "$2"
+}
 
-if  [ -n "$TMUX" ];  then
-  function set_tmux_opt {
-    tmux set -q "@$1_$TMUX_PANE" "$2"
-  }
+function get_tmux_opt {
+    tmux show -v "@$1_$2"
+}
 
-  function get_tmux_opt {
-    tmux
-  }
 
-  function get_tmux_opt {
-      tmux show -v "@$1_$2"
-  }
+function build_status () {
+# expects PANE_sTATUS_DISPLAYS to be defined
+  pane_path=$1
+  output=""
+  delimit=${PANE_STATUS_DELIMIT:-" "}
+  for display in $PANE_STATUS_DISPLAYS; do
+    output+="$(displays_dir/$display.zsh $pane_path $pane_id)$delimit"
+  done
+  output
+}
 
-  function turn_on_pane_status {
-    set_tmux_opt 'pane_status' '1'
-  }
 
-  function turn_off_pane_status {
-    set_tmux_opt 'pane_status' '0'
-  }
 
-fi
+function show_displays() {
+  pane_path=$1
+  pane_id=$2
 
+  if [ get_tmux_opt "pane_status" "$2" = "1" ]; then
+    pushd pane_path
+    echo "$(build_status $pane_path)"
+    popd
+  fi
+}
